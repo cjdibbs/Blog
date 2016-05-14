eff1ae6d8e02ce8f9a6756d54dba5c192a68b0c3
Chris Dibbs <cjdibbs@gmail.com>
2016-05-14 11:27:43 -0400

    Set up basic snap site

---
+++ site/src/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import           Control.Applicative
+import           Snap.Core
+import           Snap.Util.FileServe
+import           Snap.Http.Server
+
+main :: IO ()
+main = quickHttpServe site
+
+site :: Snap ()
+site =
+    dir "" (serveDirectory "static") <|>
+    route [ ("foo", writeBS "bar")
+          , ("echo/:echoparam", echoHandler)
+          ] 
+
+echoHandler :: Snap ()
+echoHandler = do
+    param <- getParam "echoparam"
+    maybe (writeBS "must specify echo/param in URL")
+          writeBS param
