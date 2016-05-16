a73ddf2e740eb673b902f01ba62ab45d35fa0ac2
Chris Dibbs <cjdibbs@gmail.com>
2016-05-16 18:14:20 -0400

    working idea

--- site/src/Main.hs
+++ site/src/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Applicative
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
@@ -10,14 +9,4 @@ main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site =
-    dir "" (serveDirectory "static") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] 
-
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+site = serveDirectory "static"
