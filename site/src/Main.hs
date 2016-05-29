214a961a5e834f5aecd97128f996da98990dc221
Chris Dibbs <dibbs@nowintelligence.com>
2016-05-29 18:15:17 -0400

    can download a post

--- site/src/Main.hs
+++ site/src/Main.hs
@@ -1,12 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Snap.Core
-import           Snap.Util.FileServe
-import           Snap.Http.Server
+import Snap.Core
+import Snap.Util.FileServe
+import Snap.Http.Server
+import Data.ByteString
+import Data.ByteString.UTF8 (toString)
+import Control.Applicative
+import Data.Maybe
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site = serveDirectory "static"
+site = 
+    route [ ("posts/:date/:title", getPost)
+          ] <|>
+    serveDirectory "static"
+
+getPost :: Snap ()
+getPost = do
+    date <- getParam "date"
+    title <- getParam "title"
+    respondWithPost date title
+
+failWith404 = do
+    modifyResponse $ setResponseStatus 404 "File Not Found"
+    r <- getResponse
+    finishWith r    
+
+respondWithPost :: Maybe ByteString -> Maybe ByteString -> Snap ()
+respondWithPost _       Nothing = failWith404
+respondWithPost Nothing _       = failWith404
+respondWithPost (Just d)  (Just t)  = sendFile ("posts/" ++ (toString d) ++ " - " ++ (toString t) ++ ".json")
