d86757848dcbf6f5f571ec4c5db392cfe555b0c8
Chris Dibbs <dibbs@nowintelligence.com>
2016-06-03 19:07:00 -0400

    update job works now

--- site/start-up.sh
+++ site/start-up.sh
@@ -1,3 +1,11 @@
-/opt/site/dist/build/site/site -p 80
+echo "cloning"
+cd /opt/site && git clone https://github.com/cjdibbs/Blog-Content.git posts
+echo "pulling"
+cd /opt/site/posts && /usr/bin/git pull -q origin master
+crontab /config/update-posts.txt
+crontab -l 
 cron
-tail -f /var/log/cron.log
+
+cd /opt/site
+
+/opt/site/dist/build/site/site -p 80
