4f5b677ec3b1b6c70ab5493398f56834b495593a
Chris Dibbs <dibbs@nowintelligence.com>
2016-06-05 15:33:44 -0400

    cleaned up maps and set up tag filtering

--- site/static/src/app.ts
+++ site/static/src/app.ts
@@ -1,15 +1,17 @@
-import {inject} from 'aurelia-framework';
+import {computedFrom, inject} from 'aurelia-framework';
 import 'fetch';
 import {HttpClient} from 'aurelia-fetch-client';
 import {Router, RouterConfiguration} from 'aurelia-router'
 import {Post} from 'post'
+
+type tagDatum = { tag:string, count: number};
 
 @inject(HttpClient)
 export class App {
   router: Router;
   posts: Post[];  
   client : HttpClient;
-  
+  tag: string;
   
   constructor(client: HttpClient){
     this.client = client.configure(config => {
@@ -33,10 +35,36 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = "Dibbs's Musings";
     config.map([
-      { route: ['', ':tag'], name: 'posts', moduleId: 'posts', nav: true, title: 'Home' },
+      { route: ['', ':tag'], name: 'posts', moduleId: 'posts', nav: false, title: 'Home' },
       { route: ['post/:date/:title'], name: 'post', moduleId: 'view-post', nav: false, title: "change-me"}
     ]);
     
     this.router = router;
+  }
+  
+  @computedFrom('posts', 'tag')
+  get filteredPosts(): Post[] {
+    if(!this.posts)
+      return [];
+    
+    if(this.tag)
+      return this.posts.filter(p => p.tags.indexOf(this.tag) >= 0);
+    return this.posts;
+  }
+  
+  @computedFrom('posts')
+  get tags(): tagDatum[] {
+    
+    var ret : tagDatum[] = [];
+    
+    this.posts.forEach(post => post.tags.forEach(tag => {
+      var datum = ret.find(d => d.tag === tag );
+      if(datum)
+        datum.count += 1;
+      else
+        ret.push({'tag': tag, count:1});
+    }));
+    
+    return ret.sort((a,b) => a.count - b.count);
   }
 }
--- site/static/src/posts.ts
+++ site/static/src/posts.ts
@@ -2,39 +2,21 @@ import {App} from 'app';
 import {Post} from 'post';
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
-import {MultiObserver} from 'multi-observer';
 
-@inject(App, Router, MultiObserver)
+
+@inject(App, Router)
 export class Posts {
      app: App;
      router: Router;
-     tag: string;
-     filteredPosts: Post[];
      
-     constructor(app: App, router: Router, multiObserver: MultiObserver) {
+     constructor(app: App, router: Router) {
          this.app = app;
          this.router = router;
          
-         multiObserver.observe(
-             [[this, 'tag'],
-              [this.app, 'posts']
-             ],
-             () => this.appFilter()
-        );
-     }
-     
-     appFilter(){
-         if(!this.app.posts)
-            return;
-         if(!this.tag)
-            this.filteredPosts = this.app.posts;
-         else 
-            this.filteredPosts = this.app.posts.filter(p => p.tags.indexOf(this.tag) >= 0);
      }
      
      activate(params: { tag: string}){
-         this.tag = params.tag;
-         this.appFilter();
+         this.app.tag = params.tag;
      }
      
      open(post:Post) {
